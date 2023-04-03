227e1d8fe6f8162bf34ef93731925c59e3c6236b
sandhata <hemalatha.mohan@sandhata.com>
2023-04-03 10:47:28 +0530

    first commit of calling external Api

---
+++ src/main/java/com/Hema/CallingExternalApi/CallingExternalApiApplication.java
@@ -0,0 +1,15 @@
+package com.Hema.CallingExternalApi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+
+public class CallingExternalApiApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(CallingExternalApiApplication.class, args);
+	}
+
+	
+
+}
